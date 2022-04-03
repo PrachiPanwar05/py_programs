22945544319444e261a3e68aaadc0feb63b11a17
PrachiPanwar05 <76863155+PrachiPanwar05@users.noreply.github.com>
2022-04-03 01:01:44 -0700

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,10 @@
+a = int(input("enter the value of a : "))
+b = int(input("enter the value of b : "))
+if a>b:
+  print("a is greater")
+ 
+elif a==b:
+  print("a and b are equal)
+        
+else:
+  print("b is greater")
